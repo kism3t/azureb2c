3dd1337728252dc1f96ba292421be452c036831e
Altin <ufuk.altin@kiongroup.com>
2022-02-24 14:35:22 +0000

    initial spring boot project setup

---
+++ src/main/java/com/kism3t/azureb2c/Azureb2cApplication.java
@@ -0,0 +1,13 @@
+package com.kism3t.azureb2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Azureb2cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Azureb2cApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kism3t/azureb2c/Azureb2cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kism3t.azureb2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Azureb2cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
